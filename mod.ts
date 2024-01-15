48c17fa4d3c065e0624f724b2571485e5e695caf
Jameson <james.bradlee@telenor.no>
2024-01-15 06:35:48 +0100

    Move utility files to util directory

--- mod.ts
+++ mod.ts
@@ -5,7 +5,7 @@ export { default as z } from "./lib/_deps/z.ts";
 export * from "./models/_.ts";
 
 // Utilities
-export * as print from "./lib/_print.ts";
+export * as print from "./lib/util/_print.ts";
 
 // User land
 export { defineAbstraction } from "./lib/userland/abstraction.ts";
