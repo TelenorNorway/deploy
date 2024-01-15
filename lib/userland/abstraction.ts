139e786105514e76b4a2af44fc02e49322245a49
Jameson <james.bradlee@telenor.no>
2024-01-15 05:25:50 +0100

    Add userland abstraction module

---
+++ lib/userland/abstraction.ts
@@ -0,0 +1,64 @@
+// deno-lint-ignore-file no-explicit-any ban-types
+import type { DocumentDeployType } from "deploy";
+import z from "../_deps/z.ts";
+
+export interface UserAbstraction<Metadata, Argument> {
+  readonly meta?: z.ZodSchema<Metadata>;
+  readonly arg?: z.ZodSchema<Argument>;
+  unabstract(data: {
+    arg: z.infer<z.ZodSchema<Argument>>;
+    meta: z.infer<z.ZodSchema<Metadata>>;
+  }): Promise<DocumentDeployType[]> | DocumentDeployType[];
+}
+
+export function defineAbstraction<Metadata, Argument>(
+  schemas: {
+    arg: z.ZodSchema<Argument>;
+    meta: z.ZodSchema<Metadata>;
+  },
+  unabstract: (
+    data: {
+      meta: z.infer<z.ZodSchema<Metadata>>;
+      arg: z.infer<z.ZodSchema<Argument>>;
+    },
+  ) => Promise<DocumentDeployType[]> | DocumentDeployType[],
+): UserAbstraction<Metadata, Argument>;
+
+export function defineAbstraction<Metadata>(
+  schemas: {
+    arg?: z.ZodSchema<{}>;
+    meta: z.ZodSchema<Metadata>;
+  },
+  unabstract: (
+    data: {
+      meta: z.infer<z.ZodSchema<Metadata>>;
+    },
+  ) => Promise<DocumentDeployType[]> | DocumentDeployType[],
+): UserAbstraction<Metadata, unknown>;
+
+export function defineAbstraction<Argument>(
+  schemas: {
+    arg: z.ZodSchema<Argument>;
+    meta?: z.ZodSchema<{}>;
+  },
+  unabstract: (
+    data: {
+      arg: z.infer<z.ZodSchema<Argument>>;
+    },
+  ) => Promise<DocumentDeployType[]> | DocumentDeployType[],
+): UserAbstraction<unknown, Argument>;
+
+export function defineAbstraction(
+  schemas: {
+    arg?: z.ZodSchema<{}>;
+    meta?: z.ZodSchema<{}>;
+  },
+  unabstract: () => Promise<DocumentDeployType[]> | DocumentDeployType[],
+): UserAbstraction<unknown, unknown>;
+
+export function defineAbstraction(
+  { meta, arg }: any,
+  unabstract: any,
+): UserAbstraction<any, any> {
+  return { meta, arg, unabstract };
+}
--- mod.ts
+++ mod.ts
@@ -6,3 +6,6 @@ export * from "./models/_.ts";
 
 // Utilities
 export * as print from "./lib/_print.ts";
+
+// User land
+export { defineAbstraction } from "./lib/userland/abstraction.ts";
